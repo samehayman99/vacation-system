4e37b1e633422f930a590d81f71f82ea3dedb4b0
Ahmed Osama <ahmed.osama@cch-kuwait.com>
2026-07-27 13:06:30 +0300

    update EmployeeVacationBalanceService

--- src/main/java/com/project/vacation/service/EmployeeVacationBalanceService.java
+++ src/main/java/com/project/vacation/service/EmployeeVacationBalanceService.java
@@ -1,8 +1,12 @@
 package com.project.vacation.service;
 
 import com.project.vacation.dto.EmployeeVacationBalanceResponse;
+import com.project.vacation.entity.Employee;
 import com.project.vacation.entity.EmployeeVacationBalance;
+import com.project.vacation.entity.EmployeeVacationBalanceId;
+import com.project.vacation.entity.VacationType;
 import com.project.vacation.repository.EmployeeVacationBalanceRepository;
+import com.project.vacation.repository.VacationTypeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,9 +18,9 @@ import java.util.List;
 public class EmployeeVacationBalanceService {
 
     final private EmployeeVacationBalanceRepository employeeVacationBalanceRepository;
-
+    private final VacationTypeRepository vacationTypeRepository;
     public List<EmployeeVacationBalanceResponse> getBalancesForEmployee(Long empId){
-        List<EmployeeVacationBalance> evb = employeeVacationBalanceRepository.findByEmployeeId(empId)
+        List<EmployeeVacationBalance> evb = employeeVacationBalanceRepository.findByEmployeeId(empId);
         List<EmployeeVacationBalanceResponse> result = new ArrayList<>();
 
         for(EmployeeVacationBalance empvacbalance : evb){
@@ -26,6 +30,22 @@ public class EmployeeVacationBalanceService {
         return result;
     }
 
+    public void initializeBalancesForEmployee(Employee employee) {
+        List<VacationType> types = vacationTypeRepository.findAll();
+
+        for(VacationType v : types){
+
+            EmployeeVacationBalanceId employeeVacationBalanceId = new EmployeeVacationBalanceId(employee.getId(), v.getId());
+
+            EmployeeVacationBalance employeeVacationBalance = new EmployeeVacationBalance();
+            employeeVacationBalance.setId(employeeVacationBalanceId);
+            employeeVacationBalance.setEmployee(employee);
+            employeeVacationBalance.setVacationType(v);
+            employeeVacationBalance.setDaysRemaining(v.getDaysPerYear());
+
+            employeeVacationBalanceRepository.save(employeeVacationBalance);
+        }
+    }
     private EmployeeVacationBalanceResponse toResponse(EmployeeVacationBalance balance) {
 
         return new EmployeeVacationBalanceResponse(
