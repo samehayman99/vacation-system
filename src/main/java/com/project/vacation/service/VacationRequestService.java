fdbbb0979c566cc71b890ce36b6c69075a94b28d
Ahmed Osama <ahmed.osama@cch-kuwait.com>
2026-07-27 13:47:38 +0300

    feat: add vacation request creation

--- src/main/java/com/project/vacation/service/VacationRequestService.java
+++ src/main/java/com/project/vacation/service/VacationRequestService.java
@@ -1,8 +1,15 @@
 package com.project.vacation.service;
 
+import com.project.vacation.dto.VacationRequestCreate;
 import com.project.vacation.dto.VacationRequestResponse;
+import com.project.vacation.entity.Employee;
+import com.project.vacation.entity.RequestStatus;
 import com.project.vacation.entity.VacationRequest;
+import com.project.vacation.entity.VacationType;
+import com.project.vacation.exception.ResourceNotFoundException;
+import com.project.vacation.repository.EmployeeRepository;
 import com.project.vacation.repository.VacationRequestRepository;
+import com.project.vacation.repository.VacationTypeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,8 +21,8 @@ import java.util.List;
 public class VacationRequestService {
 
     private final VacationRequestRepository vacationRequestRepository;
-
-
+    private final EmployeeRepository employeeRepository;
+    private final VacationTypeRepository vacationTypeRepository;
     public List<VacationRequestResponse> getByEmployee(Long empId){
         List<VacationRequest> requests = vacationRequestRepository.findByEmployeeId(empId);
         List<VacationRequestResponse> results = new ArrayList<>();
@@ -26,6 +33,26 @@ public class VacationRequestService {
         return results;
     }
 
+    public VacationRequestResponse Create(VacationRequestCreate dto){
+        VacationRequest vacationRequest = new VacationRequest();
+        Employee employee = employeeRepository.findById(dto.getEmpId()).orElseThrow(() -> new ResourceNotFoundException("Employee not found: " + dto.getEmpId()));;
+        vacationRequest.setEmployee(employee);
+
+        VacationType type = vacationTypeRepository.findById(dto.getVacTypeId())
+                .orElseThrow(() -> new ResourceNotFoundException("Vacation type not found: " + dto.getVacTypeId()));
+        vacationRequest.setVacationType(type);
+        vacationRequest.setEndDate(dto.getEndDate());
+        vacationRequest.setStartDate(dto.getStartDate());
+        vacationRequest.setDaysRequested(dto.getDaysRequested());
+        vacationRequest.setStatus(RequestStatus.PENDING);
+
+        return toResponse(vacationRequestRepository.save(vacationRequest));
+    }
+
+    private VacationRequest findEntityById(Long id) {
+        return vacationRequestRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Vacation type not found: " + id));
+    }
     private VacationRequestResponse toResponse(VacationRequest request) {
         VacationRequestResponse dto = new VacationRequestResponse();
         dto.setId(request.getId());
